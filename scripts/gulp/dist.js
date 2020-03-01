b27d9ac836ba283589eb31446c9552ff4db2349c
Joseph Comeau <comeau.joey@gmail.com>
2020-02-29 18:56:31 -0800

    use npm scripts

--- scripts/gulp/dist.js
+++
@@ -1,8 +0,0 @@
-
-import del from 'del';
-import gulp from 'gulp';
-import packageJSON from '../../package.json';
-
-const XYZ_VERSION = packageJSON.version;
-const DISPLAY_NAME = 'XYZ Design System';
-const MODULE_NAME = 'xyz-design-system';
